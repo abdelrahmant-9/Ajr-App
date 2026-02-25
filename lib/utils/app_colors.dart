8a614bc380c02fc123ef59d683cfbf774ab82b45
Abdelrahman Tarek <abdot9674@gmail.com>
2026-02-25 10:13:16 +0200

    Add compass image and app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.blue;
+  static const Color primaryLight = Color(0xFFBBDEFB);
+  static const Color primaryVeryLight = Color(0xFFE3F2FD);
+  static const Color red = Color(0xFFFF383C);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color green = Color(0xFF34C759);
+  static const Color lightGrey = Color(0xFFF5F5F5);
+  static const Color darkGrey = Color(0xFF64748B);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
